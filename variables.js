06721040bee97d0a2cf133f5ab85a303710aed62
Maddy Randle <maddyrandle318@gmail.com>
2023-06-01 22:35:58 -0500

    Log and reassign name variables

--- variables.js
+++ variables.js
@@ -2,3 +2,12 @@ var firstName = "Maddy"
 var lastName = "Randle"
 var hungry = true
 var tired = false
+
+console.log("first name", firstName)
+console.log("last name", lastName)
+
+firstName = "Madison"
+lastName = "Cunningham"
+
+console.log("updated first name", firstName)
+console.log("updated last name", lastName)
